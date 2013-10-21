df3f4fc41464f53aaafbeadbba37581dfb0d432d
Jean-Christophe Hoelt <hoelt@fovea.cc>
2013-10-21 15:17:56 +0300

    Allows to configure Bundle ID and IAP ID

--- test/run.sh
+++ test/run.sh
@@ -3,8 +3,34 @@
 TEST_VERSION="3.1"
 TEST_NAME="v31"
 
-#PLUGIN_URL="git://github.com/j3k0/PhoneGap-InAppPurchase-iOS.git"
-PLUGIN_URL="http://localhost/git/PhoneGap-InAppPurchase-iOS.git"
+PLUGIN_URL="git://github.com/j3k0/PhoneGap-InAppPurchase-iOS.git"
+# PLUGIN_URL="http://localhost/git/PhoneGap-InAppPurchase-iOS.git"
+
+BUNDLE_ID="$1"
+IAP_ID="$2"
+
+DIR=platforms/ios/TestIAP$TEST_NAME/Plugins/com.phonegap.plugins.inapppurchase
+WWW=platforms/ios/www/plugins/com.phonegap.plugins.inapppurchase
+PROJ=platforms/ios/TestIAP${TEST_NAME}.xcodeproj/project.pbxproj
+
+if [ "x$IAP_ID" = "x" ] || [ "x$1" = "x--help" ]; then
+    echo
+    echo "usage: $0 <bundle_id> <iap_id>"
+    echo
+    echo "This will generate a phonegap project using PhoneGap $TEST_VERSION (required)."
+    echo "If plugin install is successful, you can test your IAP as follow:"
+    echo " - if your device is logged-in a production iTunes account, go unlog (in device's settings)"
+    echo " - open ./$TEST_NAME-build/platforms/ios/TestIAP${TEST_NAME}.xcodeproj"
+    echo " - compile and run ON A DEVICE"
+    echo " - the description of the IAP should appear after a while."
+    echo " - click on the price to purchase, confirmation should appear on the console."
+    echo " - click restore and check the console that it also worked."
+    echo
+    echo "example:"
+    echo "    \$ $0 cc.fovea.babygoo babygooinapp1"
+    echo
+    exit 1
+fi
 
 # Check PhoneGap version
 V=`phonegap version`
@@ -25,6 +51,7 @@ phonegap create $TEST_NAME-build -n TestIAP$TEST_NAME -i cc.fovea.babygoo
 cp $TEST_NAME-src/css/* $TEST_NAME-build/www/css/
 cp $TEST_NAME-src/js/* $TEST_NAME-build/www/js/
 cp $TEST_NAME-src/index.html $TEST_NAME-build/www/
+sed -i "" "s/babygooinapp1/$IAP_ID/g" $TEST_NAME-build/www/js/iap.js
 
 cd $TEST_NAME-build
 
@@ -45,10 +72,6 @@ function hasFile() {
        EXIT=1
     fi
 }
-
-DIR=platforms/ios/TestIAP$TEST_NAME/Plugins/com.phonegap.plugins.inapppurchase
-WWW=platforms/ios/www/plugins/com.phonegap.plugins.inapppurchase
-PROJ=platforms/ios/TestIAP${TEST_NAME}.xcodeproj/project.pbxproj
 
 hasFile "$DIR/InAppPurchase.m"
 hasFile "$DIR/InAppPurchase.h"
