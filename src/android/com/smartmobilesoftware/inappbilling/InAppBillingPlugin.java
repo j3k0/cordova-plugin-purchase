9415f6b05ee1cecd2588a29d285876f5f9da4e49
agamemnus <agamemnus@gmail.com>
2015-01-03 23:54:07 -0500

    Update InAppBillingPlugin.java

--- src/android/com/smartmobilesoftware/inappbilling/InAppBillingPlugin.java
+++ src/android/com/smartmobilesoftware/inappbilling/InAppBillingPlugin.java
@@ -177,11 +177,7 @@ public class InAppBillingPlugin extends CordovaPlugin {
     }
 	
 	// Buy an item
-	private void buy(final String sku){
-		/* TODO: for security, generate your payload here for verification. See the comments on 
-         *        verifyDeveloperPayload() for more info. Since this is a sample, we just use 
-         *        an empty string, but on a production app you should generate this. */
-		final String payload = "";
+	private void buy(final String sku, String developerPayload){
 		
 		if (mHelper == null){
 			callbackContext.error(IabHelper.ERR_PURCHASE + "|Billing plugin was not initialized");
@@ -191,12 +187,12 @@ public class InAppBillingPlugin extends CordovaPlugin {
 		this.cordova.setActivityResultCallback(this);
 		
 		mHelper.launchPurchaseFlow(cordova.getActivity(), sku, RC_REQUEST, 
-                mPurchaseFinishedListener, payload);
+                mPurchaseFinishedListener, developerPayload);
 
 	}
 	
 	// Buy an item
-	private void subscribe(final String sku){
+	private void subscribe(final String sku, developerPayload){
 		if (mHelper == null){
 			callbackContext.error(IabHelper.ERR_PURCHASE + "|Billing plugin was not initialized");
 			return;
@@ -206,17 +202,10 @@ public class InAppBillingPlugin extends CordovaPlugin {
             return;
         }
 		
-		/* TODO: for security, generate your payload here for verification. See the comments on 
-         *        verifyDeveloperPayload() for more info. Since this is a sample, we just use 
-         *        an empty string, but on a production app you should generate this. */
-		final String payload = "";
-		
-		
-		
 		this.cordova.setActivityResultCallback(this);
         Log.d(TAG, "Launching purchase flow for subscription.");
 
-		mHelper.launchPurchaseFlow(cordova.getActivity(), sku, IabHelper.ITEM_TYPE_SUBS, RC_REQUEST, mPurchaseFinishedListener, payload);   
+		mHelper.launchPurchaseFlow(cordova.getActivity(), sku, IabHelper.ITEM_TYPE_SUBS, RC_REQUEST, mPurchaseFinishedListener, developerPayload);   
 	}
 	
 
@@ -364,11 +353,6 @@ public class InAppBillingPlugin extends CordovaPlugin {
                 return;
             }
             
-            if (!verifyDeveloperPayload(purchase)) {
-            	callbackContext.error(IabHelper.ERR_PURCHASE + "|Error purchasing. Authenticity verification failed.");
-                return;
-            }
-
             Log.d(TAG, "Purchase successful.");
             
             // add the purchase to the inventory
@@ -427,37 +411,6 @@ public class InAppBillingPlugin extends CordovaPlugin {
         else {
             Log.d(TAG, "onActivityResult handled by IABUtil.");
         }
-    }
-    
-    /** Verifies the developer payload of a purchase. */
-    boolean verifyDeveloperPayload(Purchase p) {
-        @SuppressWarnings("unused")
-		String payload = p.getDeveloperPayload();
-        
-        /*
-         * TODO: verify that the developer payload of the purchase is correct. It will be
-         * the same one that you sent when initiating the purchase.
-         * 
-         * WARNING: Locally generating a random string when starting a purchase and 
-         * verifying it here might seem like a good approach, but this will fail in the 
-         * case where the user purchases an item on one device and then uses your app on 
-         * a different device, because on the other device you will not have access to the
-         * random string you originally generated.
-         *
-         * So a good developer payload has these characteristics:
-         * 
-         * 1. If two different users purchase an item, the payload is different between them,
-         *    so that one user's purchase can't be replayed to another user.
-         * 
-         * 2. The payload must be such that you can verify it even when the app wasn't the
-         *    one who initiated the purchase flow (so that items purchased by the user on 
-         *    one device work on other devices owned by the user).
-         * 
-         * Using your own server to store and verify developer payloads across app
-         * installations is recommended.
-         */
-        
-        return true;
     }
     
     // We're being destroyed. It's important to dispose of the helper here!
