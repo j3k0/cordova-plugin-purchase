1c145cbaf36c793563668c1edd9d5aead44cc60d
Miguel Fermin <xdgimf@gmail.com>
2016-01-10 15:43:43 +0800

    Add log

--- src/js/custom.js
+++ src/js/custom.js
@@ -2,6 +2,7 @@
     "use strict";
 
     store.setApplicationUsername = function(username) {
+        store.log.info("application username - " + username);
         window.storekit.applicationUsername = username;
     };
 
