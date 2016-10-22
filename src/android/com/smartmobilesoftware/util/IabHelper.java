520ab161debcda0f3db8068409c4f3ac30ad8eaf
Alex <alexnegrila89@gmail.com>
2016-10-22 21:16:10 +0300

    Fix for common crash

--- src/android/com/smartmobilesoftware/util/IabHelper.java
+++ src/android/com/smartmobilesoftware/util/IabHelper.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.Handler;
@@ -296,7 +297,10 @@ public class IabHelper {
         Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
         serviceIntent.setPackage("com.android.vending");
         try {
-            if (!mContext.getPackageManager().queryIntentServices(serviceIntent, 0).isEmpty()) {
+            PackageManager pm = mContext.getPackageManager();
+            List<ResolveInfo> intentServices = pm.queryIntentServices(serviceIntent, 0);
+
+            if (intentServices != null && !intentServices.isEmpty()) {
                 // service available to handle that Intent
                 mContext.bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
                 return;
@@ -331,7 +335,7 @@ public class IabHelper {
         mSetupDone = false;
         if (mServiceConn != null) {
             logDebug("Unbinding from service.");
-            if (mContext != null) mContext.unbindService(mServiceConn);
+            if (mContext != null && mService != null) mContext.unbindService(mServiceConn);
         }
         mDisposed = true;
         mContext = null;
