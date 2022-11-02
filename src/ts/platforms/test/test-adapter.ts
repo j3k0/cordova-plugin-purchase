c7d1a300ba091bf056aaea2843ca9c291bd5542a
Jean-Christophe Hoelt <hoelt@fovea.cc>
2022-11-02 18:18:50 +0200

    Implement more test products in Test Adapter

--- src/ts/platforms/test/test-adapter.ts
+++ src/ts/platforms/test/test-adapter.ts
@@ -3,12 +3,14 @@ namespace CdvPurchase {
     /** Test (or Mock) Adapter and related classes */
     export namespace Test {
 
+        const platform = Platform.TEST;
+
         /** Test Adapter used for local testing with mock products */
         export class Adapter implements CdvPurchase.Adapter {
 
             id = Platform.TEST;
             name = 'Test';
-            products: Product[] = TEST_PRODUCTS;
+            products: Product[] = [];
             receipts: Receipt[] = [];
 
             private context: Internal.AdapterContext;
@@ -22,9 +24,18 @@ namespace CdvPurchase {
             async initialize(): Promise<IError | undefined> { return; }
 
             async load(products: IRegisterProduct[]): Promise<(Product | IError)[]> {
+
+                // let's test this active subscription
+                if (products.find(p => p.id === PAID_SUBSCRIPTION_ACTIVE.id)) {
+                    setTimeout(() => {
+                        this.reportActiveSubscription();
+                    }, 500); // it'll get reported in 500ms
+                }
+
                 return products.map(p => {
                     const product = TEST_PRODUCTS.find(tp => tp.id === p.id && tp.type === p.type);
-                    if (product) {
+                    if (product && this.products.indexOf(product) < 0) {
+                        this.products.push(product);
                         this.context.listener.productsUpdated(Platform.TEST, [product]);
                         return product;
                     }
@@ -36,7 +47,7 @@ namespace CdvPurchase {
 
             async order(offer: Offer): Promise<undefined | IError> {
                 // Purchasing products with "-fail-" in the id will fail.
-                if (offer.product.id.indexOf("-fail-") > 0) {
+                if (offer.id.indexOf("-fail-") > 0) {
                     return {
                         code: ErrorCode.PURCHASE,
                         message: 'Purchase failed.'
@@ -44,18 +55,17 @@ namespace CdvPurchase {
                 }
                 else {
                     // purchase succeeded, let's generate a mock receipt.
+                    const tr = new Transaction(platform);
+                    tr.products = [{
+                        productId: offer.productId,
+                        offerId: offer.id,
+                    }];
+                    tr.state = TransactionState.APPROVED;
+                    tr.transactionId = 'test-' + (new Date().getTime());
+                    tr.isAcknowledged = false;
                     const receipt = new Receipt({
-                        platform: Platform.TEST,
-                        transactions: [{
-                            platform: Platform.TEST,
-                            products: [{
-                                productId: offer.product.id,
-                                offerId: offer.id,
-                            }],
-                            state: TransactionState.APPROVED,
-                            transactionId: 'test-' + (new Date().getTime()),
-                            isAcknowledged: false,
-                        }]
+                        platform,
+                        transactions: [tr]
                     });
                     this.receipts.push(receipt);
                     this.context.listener.receiptsUpdated(Platform.TEST, [receipt]);
@@ -79,6 +89,50 @@ namespace CdvPurchase {
 
             async requestPayment(payment: PaymentRequest, additionalData?: CdvPurchase.AdditionalData): Promise<undefined | IError> {
                 return storeError(ErrorCode.UNKNOWN, 'requestPayment not supported');
+            }
+
+            private reportActiveSubscription() {
+
+                if (this.receipts.find(r => r.transactions[0].transactionId === transactionId(1))) {
+                    // already reported
+                    return;
+                }
+
+                const RENEWS_EVERY_MS = 2 * 60000; // 2 minutes
+
+                const receipt = new Receipt({
+                    platform,
+                    transactions: [],
+                });
+                function makeTransaction(n: number) {
+                    const tr = new Transaction(platform);
+                    tr.products = [{
+                        productId: PAID_SUBSCRIPTION_ACTIVE.id,
+                        offerId: PAID_SUBSCRIPTION_ACTIVE.offers[0].id,
+                    }];
+                    tr.state = TransactionState.APPROVED;
+                    tr.transactionId = transactionId(n);
+                    tr.isAcknowledged = n == 1;
+                    tr.renewalIntent = RenewalIntent.RENEW;
+                    tr.lastRenewalDate = new Date();
+                    tr.expirationDate = new Date((+(receipt?.transactions?.[0]?.expirationDate || new Date())) + RENEWS_EVERY_MS);
+                    return tr;
+                }
+                receipt.transactions.push(makeTransaction(1));
+                this.receipts.push(receipt);
+                this.context.listener.receiptsUpdated(Platform.TEST, [receipt]);
+
+                function transactionId(n: number) {
+                    return 'test-active-subscription-transaction-' + n;
+                }
+
+                let transactionNumber = 1;
+                setInterval(() => {
+                    this.log.info('auto-renewing the mock subscription')
+                    transactionNumber += 1;
+                    receipt.transactions.push(makeTransaction(transactionNumber));
+                    this.context.listener.receiptsUpdated(Platform.TEST, [receipt]);
+                }, RENEWS_EVERY_MS);
             }
         }
     }
--- src/ts/platforms/test/test-products.ts
+++ src/ts/platforms/test/test-products.ts
@@ -1,16 +1,58 @@
 namespace CdvPurchase {
   export namespace Test {
 
+    const platform = Platform.TEST;
+
     /** A consumable product for which the purchase goes through */
     export const CONSUMABLE_OK = new Product({
+      platform,
       id: 'test-consumable',
-      platform: Platform.TEST,
       type: ProductType.CONSUMABLE,
     });
     CONSUMABLE_OK.title = 'Test Consumable';
     CONSUMABLE_OK.description = 'A consumable product that you can purchase';
-    CONSUMABLE_OK.addOffer({
+    CONSUMABLE_OK.addOffer(new Offer({
       id: 'test-consumable-offer1',
+      pricingPhases: [{
+        price: '$4.99',
+        currency: 'USD',
+        priceMicros: 4990000,
+        paymentMode: PaymentMode.UP_FRONT,
+        recurrenceMode: RecurrenceMode.NON_RECURRING,
+      }],
+      product: CONSUMABLE_OK,
+    }));
+
+    /** A consumable product for which the purchase will fail */
+    export const CONSUMABLE_FAILING = new Product({
+      platform,
+      id: 'test-consumable-fail',
+      type: ProductType.CONSUMABLE,
+    });
+    CONSUMABLE_FAILING.title = 'Failing Consumable';
+    CONSUMABLE_FAILING.description = 'A consumable product that cannot be purchased';
+    CONSUMABLE_FAILING.addOffer(new Offer({
+      id: 'test-consumable-fail-offer1',
+      pricingPhases: [{
+        price: '$1.99',
+        currency: 'USD',
+        priceMicros: 1990000,
+        paymentMode: PaymentMode.UP_FRONT,
+        recurrenceMode: RecurrenceMode.NON_RECURRING,
+      }],
+      product: CONSUMABLE_FAILING,
+    }));
+
+    /** A non-consumable product */
+    export const NON_CONSUMABLE_OK = new Product({
+      platform,
+      id: 'test-non-consumable',
+      type: ProductType.NON_CONSUMABLE,
+    });
+    NON_CONSUMABLE_OK.title = 'Non Consumable';
+    NON_CONSUMABLE_OK.description = 'A non consumable product';
+    NON_CONSUMABLE_OK.addOffer(new Offer({
+      id: 'test-non-consumable-offer1',
       pricingPhases: [{
         price: '$9.99',
         currency: 'USD',
@@ -18,30 +60,66 @@ namespace CdvPurchase {
         paymentMode: PaymentMode.UP_FRONT,
         recurrenceMode: RecurrenceMode.NON_RECURRING,
       }],
-      product: CONSUMABLE_OK,
-    });
+      product: NON_CONSUMABLE_OK,
+    }));
 
-    /** A consumable product for which the purchase will fail */
-    export const CONSUMABLE_FAILING = new Product({
-      id: 'test-consumable-fail',
-      platform: Platform.TEST,
-      type: ProductType.CONSUMABLE,
+    /** A paid-subscription that auto-renews for the duration of the session */
+    export const PAID_SUBSCRIPTION_OK = new Product({
+      platform,
+      id: 'test-subscription',
+      type: ProductType.PAID_SUBSCRIPTION,
     });
-    CONSUMABLE_FAILING.title = 'Failing Consumable';
-    CONSUMABLE_FAILING.description = 'A consumable product that cannot be purchased';
-    CONSUMABLE_FAILING.addOffer({
-      id: 'test-consumable-fail-offer1',
+    PAID_SUBSCRIPTION_OK.title = 'A subscription product';
+    PAID_SUBSCRIPTION_OK.description = 'An auto-renewing paid subscription with a trial period';
+    PAID_SUBSCRIPTION_OK.addOffer(new Offer({
+      id: 'test-paid-subscription-offer1',
+      product: PAID_SUBSCRIPTION_OK,
       pricingPhases: [{
-        price: '$1.99',
+        price: '$0.00',
         currency: 'USD',
-        priceMicros: 9990000,
-        paymentMode: PaymentMode.UP_FRONT,
-        recurrenceMode: RecurrenceMode.NON_RECURRING,
+        priceMicros: 0,
+        paymentMode: PaymentMode.FREE_TRIAL,
+        recurrenceMode: RecurrenceMode.FINITE_RECURRING,
+        billingCycles: 3,
+        billingPeriod: 'P1W',
+      }, {
+        price: '$4.99',
+        currency: 'USD',
+        priceMicros: 4990000,
+        paymentMode: PaymentMode.PAY_AS_YOU_GO,
+        recurrenceMode: RecurrenceMode.INFINITE_RECURRING,
+        billingPeriod: 'P1M',
       }],
-      product: CONSUMABLE_FAILING,
+    }));
+
+    /** A paid-subscription that is already active when the app starts */
+    export const PAID_SUBSCRIPTION_ACTIVE = new Product({
+      platform,
+      id: 'test-subscription-active',
+      type: ProductType.PAID_SUBSCRIPTION,
     });
+    PAID_SUBSCRIPTION_ACTIVE.title = 'An owned subscription product';
+    PAID_SUBSCRIPTION_ACTIVE.description = 'An active paid subscription';
+    PAID_SUBSCRIPTION_ACTIVE.addOffer(new Offer({
+      id: 'test-paid-subscription-active-offer1',
+      product: PAID_SUBSCRIPTION_ACTIVE,
+      pricingPhases: [{
+        price: '$19.99',
+        currency: 'USD',
+        priceMicros: 19990000,
+        paymentMode: PaymentMode.PAY_AS_YOU_GO,
+        recurrenceMode: RecurrenceMode.INFINITE_RECURRING,
+        billingPeriod: 'P1Y',
+      }],
+    }));
 
     /** List of all test products */
-    export const TEST_PRODUCTS: Product[] = [CONSUMABLE_OK, CONSUMABLE_FAILING];
+    export const TEST_PRODUCTS: Product[] = [
+      CONSUMABLE_OK,
+      CONSUMABLE_FAILING,
+      NON_CONSUMABLE_OK,
+      PAID_SUBSCRIPTION_OK,
+      PAID_SUBSCRIPTION_ACTIVE,
+    ];
   }
 }
