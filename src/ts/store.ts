ea4a3bc4f0d5c8237f5a0a1a4f2ce7433153ba1a
Jean-Christophe Hoelt <hoelt@fovea.cc>
2022-12-13 12:29:22 +0200

    v13.0.2

--- src/ts/store.ts
+++ src/ts/store.ts
@@ -24,7 +24,7 @@ namespace CdvPurchase {
     /**
      * Current release number of the plugin.
      */
-    export const PLUGIN_VERSION = '13.0.1';
+    export const PLUGIN_VERSION = '13.0.2';
 
     /**
      * Entry class of the plugin.
